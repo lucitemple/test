6fc45cbc29d09bcba98832030d82215c2865bb6c
Luci Temple <lucitemple@yahoo.com.au>
2021-01-25 13:20:41 +1100

    Updates

---
+++ precache-manifest.c7c1830244706fad206d39c50e69845a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "328a1061cd80fd6cda97c7ae33cefdfb",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "d64ca17307e72cda2d76",
+    "url": "/home/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1e3982f2de3169f35bdd",
+    "url": "/home/static/js/2.acd7d1e7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.acd7d1e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64ca17307e72cda2d76",
+    "url": "/home/static/js/main.6ce41d09.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
